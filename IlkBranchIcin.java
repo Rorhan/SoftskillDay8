d4b4dc6678faed2a1bd21e4be8b248a5e9e50cfd
Rorhan <141484080+Rorhan@users.noreply.github.com>
2023-08-11 20:06:56 +0200

    Update IlkBranchIcin.java

--- IlkBranchIcin.java
+++ IlkBranchIcin.java
@@ -3,5 +3,6 @@ public class IlkBranchIcin {
     //Bu benim ilk commitim olucak
 
     //bu da benim ikinci commitim olucak hem de terminal ile
+    //main branchte birseyhler degisti hem de github uzerinden localden degil
 
 }
